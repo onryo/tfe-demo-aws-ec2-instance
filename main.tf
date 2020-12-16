bc84f280fbb31bd431f0090a400e5418f0c38bae
George Watkins <g@watkins.io>
2020-12-15 23:28:22 -0800

    added data source for Ubuntu AMI to automate lookups

--- main.tf
+++ main.tf
@@ -6,8 +6,24 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "ubuntu" {
-  ami                    = var.ami_id
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   availability_zone      = "${var.aws_region}b"
   key_name               = var.key_name
